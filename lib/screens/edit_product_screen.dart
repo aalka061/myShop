7a4938d0b9a80549d69e78e810f097bdacbb33f0
AK <alkafabd@gmail.com>
2021-02-06 16:29:24 -0500

    replace listview with SingleChildScrollView and Columns

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -11,15 +11,16 @@ class _EditProductScreenState extends State<EditProductScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          actions: [],
-          title: Text('Edit Product'),
-        ),
-        // it better to avoid list view column because of data loss
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Form(
-            child: ListView(
+      appBar: AppBar(
+        actions: [],
+        title: Text('Edit Product'),
+      ),
+      // it better to avoid list view column because of data loss
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          child: SingleChildScrollView(
+            child: Column(
               children: [
                 TextFormField(
                   decoration: InputDecoration(labelText: 'Title'),
@@ -28,6 +29,8 @@ class _EditProductScreenState extends State<EditProductScreen> {
               ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
