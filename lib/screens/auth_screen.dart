337d603c3b69c8b866449156eaccad149df9862e
AK <alkafabd@gmail.com>
2021-02-25 17:52:26 -0500

    Animation to change size of auth card manually

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -95,7 +95,8 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
   Map<String, String> _authData = {
@@ -104,6 +105,40 @@ class _AuthCardState extends State<AuthCard> {
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
+
+  // for animation
+  var containerHeight = 260;
+  // set animationController
+  AnimationController _controller;
+  // what do you want to animate (size)
+  Animation<Size> _heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    // give the animation controller a pointer to animation widget which
+    // should be watched
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    // animate between two values
+    _heightAnimation = Tween<Size>(
+      begin: Size(double.infinity, 260),
+      end: Size(double.infinity, 320),
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      // same speed
+      curve: Curves.linear,
+    ));
+    _heightAnimation.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
 
   void _showErrorDialog(String errorMessage) {
     showDialog(
@@ -184,10 +219,13 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.Signup;
       });
+      //starts the animation
+      _controller.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
+      _controller.reverse();
     }
   }
 
@@ -200,9 +238,9 @@ class _AuthCardState extends State<AuthCard> {
       ),
       elevation: 8.0,
       child: Container(
-        height: _authMode == AuthMode.Signup ? 320 : 260,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        // _authMode == AuthMode.Signup ? 320 : 260
+        height: _heightAnimation.value.height,
+        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
         child: Form(
