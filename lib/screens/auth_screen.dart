6de47828574f9a1c5ecef1ab06ef84baa5794167
AK <alkafabd@gmail.com>
2021-02-26 17:56:37 -0500

    use animate builder widget

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -129,15 +129,15 @@ class _AuthCardState extends State<AuthCard>
       // same speed
       curve: Curves.linear,
     ));
-    _heightAnimation.addListener(() {
-      setState(() {});
-    });
+    // _heightAnimation.addListener(() {
+    //   setState(() {});
+    // });
   }
 
   @override
   void dispose() {
     super.dispose();
-    _controller.dispose();
+    // _controller.dispose();
   }
 
   void _showErrorDialog(String errorMessage) {
@@ -237,12 +237,16 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        // _authMode == AuthMode.Signup ? 320 : 260
-        height: _heightAnimation.value.height,
-        constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, ch) => Container(
+          // _authMode == AuthMode.Signup ? 320 : 260
+          height: _heightAnimation.value.height,
+          constraints: BoxConstraints(minHeight: _heightAnimation.value.height),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: ch,
+        ),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
